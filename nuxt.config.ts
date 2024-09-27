9f76ba9a294aabc116e2723d21b0bbd1ecb7d96b
roudai <me@roudai.net>
2024-09-28 00:12:44 +0900

    Github Pages設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    cdnURL: '/<repository>/'
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint']
